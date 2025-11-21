aa6f40fdbb07e63390e79bbaef479658b8046257
dp-record <dp-record@deliberate-programming.org>
2025-11-21 14:58:29 +0100

    Auto-commit after 1.0min

--- src/test/java/ch/css/m3000/banking/AccountTest.java
+++ src/test/java/ch/css/m3000/banking/AccountTest.java
@@ -4,6 +4,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Your Task
+ * Your bank is tired of its mainframe COBOL accounting software and they hired both of you for a greenfield project in - what a happy coincidence
+ * <p>
+ * your favorite programming language!
+ * Your task is to show them that your TDD-fu and your new-age programming language can cope with good ole’ COBOL!
+ * <p>
+ * Requirements
+ * Write a class Account that offers the following methods void deposit(int) void withdraw(int) String printStatement()
+ * <p>
+ * An example statement would be:
+ * <p>
+ * Date        Amount  Balance
+ * 24.12.2015   +500      500
+ * 23.8.2016    -100      400
+ */
 public class AccountTest {
     @Test
     void dada() {
