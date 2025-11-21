63ec897bfd8ee0dc281672e088700e1b92b29dc7
dp-record <dp-record@deliberate-programming.org>
2025-11-21 14:56:28 +0100

    Auto-commit after 1.0min

--- src/test/java/ch/css/m3000/banking/AccountTest.java
+++ src/test/java/ch/css/m3000/banking/AccountTest.java
@@ -1,3 +1,9 @@
+package ch.css.m3000.banking;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AccountTest {
     @Test
     void dada() {
